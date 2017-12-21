8ecc05317dce61cb5706196315d52e7a19471cea
William Reynolds <william.g.reynolds@egadsthefuzz.com>
2017-12-21 11:53:18 +1100

    v6 type updates

--- spec/inputs/snmp_spec.rb
+++ spec/inputs/snmp_spec.rb
@@ -3,7 +3,6 @@ require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/snmp"
 
 describe LogStash::Inputs::Snmp do
-
   it_behaves_like "an interruptible input plugin" do
     let(:config) { { "interval" => 100 } }
   end
